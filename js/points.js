b7b6304c5dbc25630626da45e2d6f7ca8c788910
Ben <me@vnbenny.com>
2016-10-24 01:15:40 +0200

    fix

--- js/points.js
+++ js/points.js
@@ -58,7 +58,7 @@ var cardButtonCallback = function(t) {
       callback: function(t) {
           var member = t.args['0'].context.member;
           return t.get('card', 'shared', 'points').then(function(points) {
-              if(point = 0) delete points[member];
+              if(point == 0) delete points[member];
               else points[member] = point;
               return t.set('card', 'shared', 'points', points).then(function() {
                   return t.closePopup();
